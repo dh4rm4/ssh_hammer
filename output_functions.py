998da9fd02f68c39c9d897fba1e99a41d8221225
dh4rm4 <dh4rm4@protonmail.com>
2019-01-04 12:46:30 +0100

    transform output functions into a uniq class

--- output_functions.py
+++ output_functions.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""
+Class used to manage all output messages
+"""
 
-class col:
+class output():
     LOWRED      = '\033[94m'
     OKGREEN     = '\033[92m'
     YELLOW      = '\033[93m'
@@ -9,54 +12,53 @@ class col:
     BOLD        = '\033[1m'
     UNDERLINE   = '\033[4m'
 
+    def print_front(self, c, color):
+        print(self.BOLD + '[' + color + c + self.ENDC + \
+               self.BOLD + '] ' + self.ENDC, end='')
 
-###################################
-#                                 #
-#      Output functions           #
-#                                 #
-###################################
+    def print_error(self):
+        self.print_front('-', self.FAIL)
+        print(self.FAIL + "Error" + self.ENDC + ': ', end='')
 
-def printError():
-    printFront('-', col.FAIL)
-    print (col.FAIL + "Error" + col.ENDC + ': ', end='')
+    def printConnectFail(self, user, passwd):
+        self.print_front('-', self.FAIL)
+        print('Fail connecting to host: ' + self.LOWRED + \
+               user + self.ENDC + ':' + self.FAIL + passwd + \
+               self.ENDC)
 
-def printFront(c, color):
-    print (col.BOLD + '[' + color + c + col.ENDC + col.BOLD + '] ' + col.ENDC, end='')
+    def file_does_not_exist(self, filename):
+        self.print_error()
+        print(filename + " file does not exist")
+        return 1
 
-def printConnectFail(user, passwd):
-    printFront('-', col.FAIL)
-    print ('Fail connecting to host: ' + col.LOWRED + user + col.ENDC + ':' + col.FAIL + passwd + col.ENDC)
+    def wrong_perm_on_file(self, filename):
+        self.print_error()
+        print("you don't have the right to read " + filename)
 
-def fileDoNotExist(filename):
-    printError()
-    print (filename + " file does not exist")
-    return 1
-
-def wrongPermOnFile(filename):
-    printError()
-    print ("you don't have the right to read " + filename)
-
-def printHello():
-    print (col.BOLD + "\n\
+    def hello(self):
+        print(self.BOLD + "\n\
                                             \`.     \n\
                   .--------------.___________) \         \n\
-                  |" + col.LOWRED + '//////////////' +
-           col.ENDC + col.BOLD + "|___________[ ]         \n\
+                  |" + self.LOWRED + '//////////////' +
+           self.ENDC + self.BOLD + "|___________[ ]         \n\
                   `--------------'           ) (         \n\
                                             \\'-'/        " +
-           col.ENDC)
-    print (col.BOLD + col.FAIL + '                   	          HAMMER     SSH      \n' + col.ENDC)
+           self.ENDC)
+        print(self.BOLD + self.FAIL + \
+               '                   	          HAMMER     SSH      \n' + \
+               self.ENDC)
 
-def printSuccess(passwd, user):
-    print ('\n')
-    printFront('†', col.OKGREEN)
-    print(col.BOLD + 'The SSH gate collapse' + col.ENDC)
-    printFront('†', col.OKGREEN)
-    print ('user: ' + col.BOLD + col.OKGREEN + user +
-           col.ENDC + ' | password: \"' + col.BOLD +
-           col.OKGREEN + passwd + '\"\n' + col.ENDC)
+    def printSuccess(self, passwd, user):
+        print('\n')
+        self.print_front('†', self.OKGREEN)
+        print(self.BOLD + 'The SSH gate collapse' + self.ENDC)
+        self.print_front('†', self.OKGREEN)
+        print('user: ' + self.BOLD + self.OKGREEN + user +
+               self.ENDC + ' | password: \"' + self.BOLD +
+               self.OKGREEN + passwd + '\"\n' + self.ENDC)
 
-def printWhereFindOutput():
-    print ("\n\n")
-    printFront('!', col.LOWRED)
-    print ("      " + col.BOLD + "OUTPUT IN: " + col.FAIL + col.UNDERLINE + "crackingResult.passwd" + col.ENDC)
+    def printWhereFindOutput(self):
+        print("\n\n")
+        self.print_front('!', self.LOWRED)
+        print("      " + self.BOLD + "OUTPUT IN: " + self.FAIL + \
+               self.UNDERLINE + "crackingResult.passwd" + self.ENDC)
