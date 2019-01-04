44205a2aeb5d8e4f2fbd67bd0bda2e4ed001ea3a
dh4rm4 <dh4rm4@protonmail.com>
2019-01-04 12:59:45 +0100

    pep8

--- output_functions.py
+++ output_functions.py
@@ -3,18 +3,19 @@
 Class used to manage all output messages
 """
 
+
 class output():
-    LOWRED      = '\033[94m'
-    OKGREEN     = '\033[92m'
-    YELLOW      = '\033[93m'
-    FAIL        = '\033[91m'
-    ENDC        = '\033[0m'
-    BOLD        = '\033[1m'
-    UNDERLINE   = '\033[4m'
+    LOWRED = '\033[94m'
+    OKGREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
     def print_front(self, c, color):
-        print(self.BOLD + '[' + color + c + self.ENDC + \
-               self.BOLD + '] ' + self.ENDC, end='')
+        print(self.BOLD + '[' + color + c + self.ENDC +
+              self.BOLD + '] ' + self.ENDC, end='')
 
     def print_error(self):
         self.print_front('-', self.FAIL)
@@ -22,9 +23,9 @@ class output():
 
     def printConnectFail(self, user, passwd):
         self.print_front('-', self.FAIL)
-        print('Fail connecting to host: ' + self.LOWRED + \
-               user + self.ENDC + ':' + self.FAIL + passwd + \
-               self.ENDC)
+        print('Fail connecting to host: ' + self.LOWRED +
+              user + self.ENDC + ':' + self.FAIL + passwd +
+              self.ENDC)
 
     def file_does_not_exist(self, filename):
         self.print_error()
@@ -39,14 +40,17 @@ class output():
         print(self.BOLD + "\n\
                                             \`.     \n\
                   .--------------.___________) \         \n\
-                  |" + self.LOWRED + '//////////////' +
-           self.ENDC + self.BOLD + "|___________[ ]         \n\
+                  |" +
+              self.LOWRED +
+              '//////////////' +
+              self.ENDC +
+              self.BOLD + "|___________[ ]         \n\
                   `--------------'           ) (         \n\
                                             \\'-'/        " +
-           self.ENDC)
-        print(self.BOLD + self.FAIL + \
-               '                   	          HAMMER     SSH      \n' + \
-               self.ENDC)
+              self.ENDC)
+        print(self.BOLD + self.FAIL +
+              '                   	          HAMMER     SSH      \n' +
+              self.ENDC)
 
     def printSuccess(self, passwd, user):
         print('\n')
@@ -54,11 +58,16 @@ class output():
         print(self.BOLD + 'The SSH gate collapse' + self.ENDC)
         self.print_front('†', self.OKGREEN)
         print('user: ' + self.BOLD + self.OKGREEN + user +
-               self.ENDC + ' | password: \"' + self.BOLD +
-               self.OKGREEN + passwd + '\"\n' + self.ENDC)
+              self.ENDC + ' | password: \"' + self.BOLD +
+              self.OKGREEN + passwd + '\"\n' + self.ENDC)
 
     def printWhereFindOutput(self):
         print("\n\n")
         self.print_front('!', self.LOWRED)
-        print("      " + self.BOLD + "OUTPUT IN: " + self.FAIL + \
-               self.UNDERLINE + "crackingResult.passwd" + self.ENDC)
+        print("      " +
+              self.BOLD +
+              "OUTPUT IN: " +
+              self.FAIL +
+              self.UNDERLINE +
+              "crackingResult.passwd" +
+              self.ENDC)
